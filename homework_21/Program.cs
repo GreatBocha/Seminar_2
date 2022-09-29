ab71a6ad93e334904c863a697eb53f6b7ae6d4d5
Sergey <grushevskiy.1993@gmail.com>
2022-09-29 21:20:30 +0700

    homework21

---
+++ homework_21/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//  Напишите программу, которая принимает на вход координаты двух
+//  точек и находит расстояние между ними в 3D пространстве.
+
+void Piff()
+{
+    Random random = new Random();
+    double aX =  random.Next(1,50);
+    double aY =  random.Next(1,50);
+    double aZ =  random.Next(1,50);
+    double bX =  random.Next(1,50);
+    double bY =  random.Next(1,50);
+    double bZ =  random.Next(1,50);
+
+    CoordRange(aX, aY, bX, bY, aZ, bZ);
+}
+
+void CoordRange(double aX, double aY, double bX, double bY, double aZ, double bZ)
+{
+    double range = Math.Sqrt(Math.Pow(aX - bX, 2) + Math.Pow(aY - bY, 2) + Math.Pow(aZ - bZ, 2));
+    Console.WriteLine("расстояние между двумя точками " + (Math.Round(range, 2)));
+}
+
+Piff();
